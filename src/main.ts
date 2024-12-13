3852d816960f5f80e0753c2226ceda3a4e4d6f73
max-catalyst <my@wincoll.ac.uk>
2024-12-13 17:20:03 +0000

    MMI prep

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ let currentPopup: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
+	WA.controls.disableRoomList();
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
